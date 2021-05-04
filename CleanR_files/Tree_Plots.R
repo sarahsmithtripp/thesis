dbbd9e98601fec165bdc531451603d5346492ef9
sarahsmithtripp <sarahsmith.tripp@gmail.com>
2021-05-04 15:55:02 -0700

    Updated Tree plots to run in Metadata folder

--- CleanR_files/Tree_Plots.R
+++ CleanR_files/Tree_Plots.R
@@ -1,19 +1,17 @@
 ##SST 
 ##OCt-18-2020
 #Code written to plot output from the field tree height surveys
-setwd("D:/Data/SmithTripp/Gavin_Lake/Field_SiteData/Tree_data")
+# Set working directory to where metadata is stored 
+#setwd("F:/SmithTripp_Metadata")
 
 #necessary packages to run 
 library(ggplot2)
 library(reshape2)
 library(rgeos)
 library(rgdal)
-
 library(raster)
-
 library(dplyr)
 library(readxl)
-
 library(cowplot)
 
 ##write function to convert polar to cartesian 
@@ -28,7 +26,7 @@ polar2cart <- function(data) {
 }
 
 ## read in the data
-trees <- readxl::read_excel("Tree_Data.xlsx", sheet = "Cleaned")
+trees <- readxl::read_excel("_Field-Collected-Data/Tree_Plots/Tree_Data.xlsx", sheet = "Cleaned")
 trees$Micro_plot_fact <- trees$`Microclimate Plot`
 trees <- trees %>% 
   mutate(plot_match = dplyr::case_when(.$'Micro_plot_fact'== '60' ~ "60",
@@ -47,7 +45,7 @@ trees <- trees %>%
 #write sequence to name for numbers 
 seq <- seq(from =1 , to = 10, by = 1)
 fs_seq <- paste0(seq)
-plots <- rgdal::readOGR("D:/Data/SmithTripp/Gavin_Lake/Field_SiteData/Sample_Location/Plots.shp")
+plots <- rgdal::readOGR("_field-site-data/Plots.shp")
 plots@data <- plots@data %>% 
   arrange(X_firemean) %>%
   mutate(
@@ -135,15 +133,18 @@ map
 # Read in tree plot gps locations  ----------------------------------------
 
 # read in field data locations 
-tree_locations_partial <- rgdal::readOGR(list.files(getwd())[which(grepl("Tree_plots_NAD27.shp",list.files(getwd())))])
+tree_locations_partial <- rgdal::readOGR("_Field-Collected-Data/Tree_Plots/tree_locations/tree_locations.shp")
+
 plot_12_coords <- cbind(585179.4209196416,5811225.1762742680)
-plot_12_data  <- data.frame(t(c(1, "tree_12", NA, NA, 2020-10-10,585164.4209196416, 5811240.1762742680)))
+plot_12_data  <- data.frame(t(c(15, "tree_12",NA, NA, 2020-10-10,585164.4209196416, 5811240.1762742680)))
 names(plot_12_data) <- names(tree_locations_partial@data)
 #combine plot 12 with other plots 
 data_merge <- rbind(tree_locations_partial@data, plot_12_data)
 
 #create spatial polygons data frame to work with the data 
 tree_locations <- sp::SpatialPointsDataFrame(cbind(as.numeric(data_merge[,'x']),as.numeric(data_merge[,'y'])) ,data = data_merge)
+
+#set shapefile locations 
 sp::proj4string(tree_locations)<- sp::CRS("+proj=utm +zone=10 +datum=NAD83 +units=m +no_defs")
 #export shapefile and double check locations in QGIS 
 #rgdal::writeOGR(tree_locations, "tree_locations", layer= "tree_locations", driver = "ESRI Shapefile", overwrite_layer = T)
@@ -162,23 +163,18 @@ raster::plot(tree_location_buffer)
 # the next uses a raster that goes to 0 
 #finally there is a raster with a lower resolution (closer to what will be input in the microclimate models )
 # create list of file locations  ------------------------------------------
-query <- list.files(paste0("D:/Data/SmithTripp/Gavin_Lake/3D_models"), full.names = T)
-file_folders <- file_folders <- list(query[[5]], ## lowerplots
-                                     query[[6]], ## midplots
-                                     query[[8]], ##old_guy
-                                     query[[14]]) ## top plots
+query <- list.files(paste0("_RPA-Aqcuired-Data/Canopy_Structure/Canopy_Height/"), full.names = T)
+file_folders <- file_folders <- list(query[[1]], ## lowerplots
+                                     query[[2]], ## midplots
+                                     query[[3]], ##old_guy
+                                     query[[4]]) ## top plots
 all_files <- unlist(lapply(file_folders, list.files, full.names = T))
 
 #raster with no values below 2m -> possibly more accurate to canopy height 
-chm_2m_raster <- raster::raster(paste0(query[[4]], 
-                                      "/chm_2m_10cmres.tif"))
-
-#raster with all values (heights = 0 to max) used to produce chm
-chm_tif <- all_files[which(grepl("/Old_guy/chm_10cm.tif", all_files))][1]
-chm_raster <- raster::raster(chm_tif)
+chm_raster <- raster::raster(paste0(query[[3]])) ## all of the values included here 
 
 #Alternative to aggregate to 2 meter resolution
-chm_raster_2m <- raster::aggregate(chm_raster, 20, fun = mean, na.rm = T)
+# chm_raster_2m <- raster::aggregate(chm_raster, 20, fun = mean, na.rm = T)
 
 ## plot locations on CHMs
 # plot.new()
@@ -212,30 +208,30 @@ DAP_comp_Field <- function(raster, field_data,variable_of_interest,
 
 
 chm_raster[chm_raster == 0] <- NA
-chm_2m_raster[chm_2m_raster == 0] <- NA
-chm_raster_2m[chm_raster_2m == 0]
+#chm_2m_raster[chm_2m_raster == 0] <- NA
+#chm_raster_2m[chm_raster_2m == 0]
 
 # Insitu sampling took place within 6.5 radius plots
 # Note that below will return a dataframe containing the max height
 # calculated from all pixels in the buffer for each plot
-CHM_max_height <- raster::extract(chm_2m_raster,
+CHM_max_height <- raster::extract(chm_raster,
                        tree_locations,
                        buffer = 6.5,
                        fun=max,
                        sp=TRUE,
                        stringsAsFactors=FALSE)
-CHM_mean_height <- raster::extract(chm_2m_raster,
+CHM_mean_height <- raster::extract(chm_raster,
                                   tree_locations,
                                   buffer = 6.5,
                                   fun=mean,
                                   sp=TRUE,
                                   stringsAsFactors=FALSE)
-CHM_max_height_2m <- raster::extract(chm_raster_2m,
-                                  tree_locations,
-                                  buffer = 6.5,
-                                  fun=max,
-                                  sp=TRUE,
-                                  stringsAsFactors=FALSE)
+# CHM_max_height_2m <- raster::extract(chm_raster_2m,
+#                                   tree_locations,
+#                                   buffer = 6.5,
+#                                   fun=max,
+#                                   sp=TRUE,
+#                                   stringsAsFactors=FALSE)
 
 
 #function to clean up naming of plots for easier field vs. DAP deduction
@@ -250,7 +246,7 @@ clean_naming <- function(data){
 
 
 CHM_max_height <- clean_naming(CHM_max_height)
-CHM_max_height_2m <- clean_naming(CHM_max_height_2m)
+#CHM_max_height_2m <- clean_naming(CHM_max_height_2m)
 CHM_mean_height <- clean_naming(CHM_mean_height)
 
 
@@ -263,7 +259,7 @@ levels(CHM_mean_field$plot_num)
 CHM_field_mean_livetrees <- merge(CHM_mean_height, summary_livetrees, by = "Plot")
 CHM_field_2m <- merge(CHM_max_height_2m, summary_trees, by = "Plot")
 ## plot the derived data
-library(ggplot2)
+
 ## added values ot align field (which includes data taken at 1.6 m)
 max_plot <- ggplot(CHM_max_field@data, aes(x=chm_2m_10cmres+1.6, y = fieldhtmax_trees)) +
   geom_point(size = 3) + 
@@ -305,9 +301,8 @@ mean_plot <- ggplot(chm_mean_field_clean, aes(x=chm_2m_10cmres, y = fieldhtmean_
   cowplot::theme_cowplot() + ggtitle(c("Mean DAP vs. Field Ht"))
 mean_plot
 
-save_plot('D:/Data/SmithTripp/Gavin_Lake/Figures/HeightVerification_Plots.jpg',
-          cowplot::plot_grid(max_plot, mean_plot, rel_widths = c(1.2, 1.5)),
-          base_width =7.5, base_height = 4)
+# save_plot('D:/Data/SmithTripp/Gavin_Lake/Figures/HeightVerification_Plots.jpg',
+cowplot::plot_grid(max_plot, mean_plot, rel_widths = c(1.2, 1.5),base_width =7.5, base_height = 4)
 
 
 lm_tree_height_max <- lm(fieldhtmax_trees ~ chm_2m_10cmres , CHM_max_field@data)
